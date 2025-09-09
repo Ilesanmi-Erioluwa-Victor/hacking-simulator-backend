3180f6e03562db92b7b5ca40d7a888542fe0cb74
ilesanmi erioluwa victor <ilesanmierioluwavictor@gmail.com>
2025-09-10 00:22:05 +0100

    Remove .env from repo

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const authRoutes = require("./routes/auth");
 const scanRoutes = require("./routes/scan");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(helmet()); // Security headers
